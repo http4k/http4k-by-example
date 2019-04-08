d8aca6274b9509c178ef6e703fa84d986296ac1a
David Denton <denton.david@gmail.com>
2019-04-08 17:32:17 +0100

    convert contract to standard routings..

--- src/test/kotlin/env/FakeEntryLogger.kt
+++ src/test/kotlin/env/FakeEntryLogger.kt
@@ -1,41 +1,55 @@
 package env
 
-import org.http4k.contract.bindContract
-import org.http4k.contract.contract
 import org.http4k.core.Body
 import org.http4k.core.HttpHandler
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
 import org.http4k.core.Status.Companion.ACCEPTED
 import org.http4k.core.Status.Companion.CREATED
 import org.http4k.core.with
 import org.http4k.format.Jackson.auto
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import verysecuresystems.UserEntry
 
 class FakeEntryLogger : HttpHandler {
 
-    private val userEntry = Body.auto<UserEntry>().toLens()
-    private val userEntries = Body.auto<List<UserEntry>>().toLens()
-
     val entries = mutableListOf<UserEntry>()
 
-    private val app = contract {
-        routes += "/entry" bindContract Method.POST to { req: Request ->
+    private fun list(): HttpHandler {
+        val userEntry = Body.auto<UserEntry>().toLens()
+        return { req ->
             val entry = userEntry(req)
             entries += entry
             Response(CREATED).with(userEntry of entry)
         }
-        routes += "/exit" bindContract Method.POST to { req: Request ->
+    }
+
+    private fun entry(): HttpHandler {
+        val userEntry = Body.auto<UserEntry>().toLens()
+        return { req ->
+            val entry = userEntry(req)
+            entries += entry
+            Response(CREATED).with(userEntry of entry)
+        }
+    }
+
+    private fun exit(): HttpHandler {
+        val userEntry = Body.auto<UserEntry>().toLens()
+        return { req ->
             val entry = userEntry(req)
             entries += entry
             Response(ACCEPTED).with(userEntry of entry)
         }
-        routes += "/list" bindContract Method.GET to {
-            Response(Status.OK).with(userEntries of entries)
-        }
     }
+
+    private val app = routes(
+        "/list" bind GET to list(),
+        "/entry" bind POST to entry(),
+        "/exit" bind POST to exit()
+    )
 
     override fun invoke(p1: Request) = app(p1)
 }
