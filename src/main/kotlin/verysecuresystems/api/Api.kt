0ae739f551322bce86b03c0052e8fee996338bf1
David Denton <denton.david@gmail.com>
2019-05-25 22:29:02 +0100

    upgrade

--- src/main/kotlin/verysecuresystems/api/Api.kt
+++ src/main/kotlin/verysecuresystems/api/Api.kt
@@ -7,6 +7,7 @@ import org.http4k.contract.security.ApiKeySecurity
 import org.http4k.core.then
 import org.http4k.filter.CorsPolicy.Companion.UnsafeGlobalPermissive
 import org.http4k.filter.ServerFilters.Cors
+import org.http4k.format.Jackson
 import org.http4k.lens.Header
 import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
@@ -18,7 +19,7 @@ object Api {
     operator fun invoke(userDirectory: UserDirectory, entryLogger: EntryLogger, inhabitants: Inhabitants): RoutingHttpHandler =
         "/api" bind Cors(UnsafeGlobalPermissive)
             .then(contract {
-                renderer = OpenApi3(ApiInfo("Security server API - the API key is 'realSecret'!", "v1.0"))
+                renderer = OpenApi3(ApiInfo("Security server API - the API key is 'realSecret'!", "v1.0"), Jackson)
                 descriptionPath = "/api-docs"
                 security = ApiKeySecurity(Header.required("key"), { key: String -> key == "realSecret" })
                 routes += KnockKnock(userDirectory::lookup, inhabitants::add, entryLogger::enter)
