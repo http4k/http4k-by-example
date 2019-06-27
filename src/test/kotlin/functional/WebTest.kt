eb2ce577fc623568f197290296901dcd227dcebd
David Denton <denton.david@gmail.com>
2019-06-27 19:42:26 +0100

    expect 200 from browser

--- src/test/kotlin/functional/WebTest.kt
+++ src/test/kotlin/functional/WebTest.kt
@@ -2,10 +2,12 @@ package functional
 
 import env.TestEnvironment
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.I_M_A_TEAPOT
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Uri
 import org.http4k.testing.ApprovalTest
 import org.http4k.testing.Approver
+import org.http4k.testing.assertApproved
 import org.http4k.webdriver.Http4kWebDriver
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -23,7 +25,7 @@ class WebTest {
     }
 
     @Test
-    fun `manage users`(approver: Approver) {
+    fun `manage users login via oauth`(approver: Approver) {
         approver.assertApproved(
             browser.apply {
                 get(Uri.of("/users"))
@@ -39,5 +41,5 @@ class WebTest {
 }
 
 private fun Approver.assertApproved(browser: Http4kWebDriver) {
-    assertApproved(Response(OK).body(browser.pageSource ?: ""))
+    assertApproved(Response(browser.status ?: I_M_A_TEAPOT).body(browser.pageSource ?: ""), OK)
 }
