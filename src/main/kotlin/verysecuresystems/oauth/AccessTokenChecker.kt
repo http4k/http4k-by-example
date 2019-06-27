c5e933d989c1bb6dc9c2754f6b9ebc8606ca977c
David Denton <denton.david@gmail.com>
2019-06-27 20:07:05 +0100

    refactor

--- src/main/kotlin/verysecuresystems/oauth/AccessTokenChecker.kt
+++ src/main/kotlin/verysecuresystems/oauth/AccessTokenChecker.kt
@@ -6,7 +6,5 @@ typealias AccessTokenChecker = (AccessToken) -> Boolean
 
 object InsecureAccessTokenChecker : AccessTokenChecker {
     const val VALID_PREFIX = "AUTH_TOKEN_AUTH_CODE_"
-    override operator fun invoke(token: AccessToken): Boolean {
-        return token.value.startsWith(VALID_PREFIX)
-    }
+    override operator fun invoke(token: AccessToken) = token.value.startsWith(VALID_PREFIX)
 }
