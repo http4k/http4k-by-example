0657ed305c6bb708e4b47d59d2c59cd490ec24ed
David Denton <denton.david@gmail.com>
2019-06-27 21:24:31 +0100

    test for start/stop

---
+++ src/test/kotlin/unit/SecuritySystemServerTest.kt
@@ -0,0 +1,27 @@
+package unit
+
+import org.http4k.cloudnative.env.Environment
+import org.http4k.core.Uri
+import org.junit.jupiter.api.Test
+import verysecuresystems.SecuritySystemServer
+import verysecuresystems.Settings
+
+class SecuritySystemServerTest {
+
+    @Test
+    fun `can build server and start and stop it`() {
+        val securityServerPort = 9000
+        val userDirectoryPort = 10000
+        val entryLoggerPort = 11000
+        val oauthServerPort = 12000
+
+        val env = Environment.defaults(Settings.PORT of securityServerPort,
+            Settings.SECURITY_SERVER_URL of Uri.of("http://localhost:$securityServerPort"),
+            Settings.USER_DIRECTORY_URL of Uri.of("http://localhost:$userDirectoryPort"),
+            Settings.ENTRY_LOGGER_URL of Uri.of("http://localhost:$entryLoggerPort"),
+            Settings.OAUTH_SERVER_URL of Uri.of("http://localhost:$oauthServerPort")
+        )
+
+        SecuritySystemServer(env).start().stop()
+    }
+}
