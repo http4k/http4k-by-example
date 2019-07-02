df2ae4dcd99a33ffe0f2e4e0ca48897704fec025
David Denton <denton.david@gmail.com>
2019-07-02 07:41:33 +0100

    remove CORs policy as is not required when we are accessing the API from the bundled OpenApi UI

--- src/main/kotlin/verysecuresystems/api/Api.kt
+++ src/main/kotlin/verysecuresystems/api/Api.kt
@@ -5,10 +5,8 @@ import org.http4k.contract.openapi.ApiInfo
 import org.http4k.contract.openapi.v3.OpenApi3
 import org.http4k.contract.security.OAuthSecurity
 import org.http4k.core.Method.GET
-import org.http4k.core.then
-import org.http4k.filter.CorsPolicy.Companion.UnsafeGlobalPermissive
-import org.http4k.filter.ServerFilters.Cors
 import org.http4k.format.Jackson
+import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.security.OAuthProvider
@@ -16,24 +14,25 @@ import verysecuresystems.Inhabitants
 import verysecuresystems.external.EntryLogger
 import verysecuresystems.external.UserDirectory
 
+/**
+ * The exposed Server API, protected by Bearer Token.
+ */
 object Api {
     operator fun invoke(userDirectory: UserDirectory,
                         entryLogger: EntryLogger,
                         inhabitants: Inhabitants,
                         oAuthProvider: OAuthProvider
-    ) = Cors(UnsafeGlobalPermissive)
-        .then(
-            "/api" bind routes(
-                "/oauth/callback" bind GET to oAuthProvider.callback,
-                contract {
-                    renderer = OpenApi3(ApiInfo("Security Server API", "v1.0", "This API is secured by an OAuth auth code. Simply click 'Authorize' to start the flow."), Jackson)
-                    descriptionPath = "/api-docs"
-                    security = OAuthSecurity(oAuthProvider)
-                    routes += KnockKnock(userDirectory::lookup, inhabitants::add, entryLogger::enter)
-                    routes += WhoIsThere(inhabitants, userDirectory::lookup)
-                    routes += ByeBye(inhabitants::remove, entryLogger::exit)
-                }
-            )
+    ): RoutingHttpHandler =
+        "/api" bind routes(
+            "/oauth/callback" bind GET to oAuthProvider.callback,
+            contract {
+                renderer = OpenApi3(ApiInfo("Security Server API", "v1.0", "This API is secured by an OAuth auth code. Simply click 'Authorize' to start the flow."), Jackson)
+                descriptionPath = "/api-docs"
+                security = OAuthSecurity(oAuthProvider)
+                routes += KnockKnock(userDirectory::lookup, inhabitants::add, entryLogger::enter)
+                routes += WhoIsThere(inhabitants, userDirectory::lookup)
+                routes += ByeBye(inhabitants::remove, entryLogger::exit)
+            }
         )
 }
 
