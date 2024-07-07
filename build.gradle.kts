44f0f0e48ac2230f79ccd76c66950f8f9e1d84db
David Denton <denton.david@gmail.com>
2024-07-07 13:10:18 +0100

    Update everything to new versions

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.gradle.api.JavaVersion.VERSION_21
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+val http4kVersion: String by project
+val junitVersion: String by project
+
+repositories {
+    mavenCentral()
+}
+
+apply(plugin = "kotlin")
+
+tasks {
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+            jvmTarget.set(JVM_21)
+            freeCompilerArgs.add("-Xjvm-default=all")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    java {
+        sourceCompatibility = VERSION_21
+        targetCompatibility = VERSION_21
+    }
+}
+
+dependencies {
+
+    implementation(platform("org.http4k:http4k-bom:$http4kVersion"))
+
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-client-okhttp")
+    implementation("org.http4k:http4k-cloudnative")
+    implementation("org.http4k:http4k-contract")
+    implementation("org.http4k:http4k-format-jackson")
+    implementation("org.http4k:http4k-security-oauth")
+    implementation("org.http4k:http4k-server-undertow")
+    implementation("org.http4k:http4k-template-handlebars")
+
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.http4k:http4k-testing-hamkrest")
+    testImplementation("org.http4k:http4k-testing-chaos")
+    testImplementation("org.http4k:http4k-testing-approval")
+    testImplementation("org.http4k:http4k-testing-webdriver")
+}
